edf3ecf7c77718cdeaca906b3bff751dd1330e0e
Lawrence Onah <paplow01@gmail.com>
2026-05-01 12:08:31 +0100

    style(core): sort cache barrel exports alphabetically (lint autofix)

--- packages/core/src/cache/index.ts
+++ packages/core/src/cache/index.ts
@@ -1,5 +1,3 @@
-export * from './cache-strategy.interface';
-
-export * from './mock-cache-strategy';
-
 export * from './cache-key.util';
+export * from './cache-strategy.interface';
+export * from './mock-cache-strategy';
